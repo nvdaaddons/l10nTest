1676d8fc16194b67a318ba19383c48c0530d9961
Mesar Hameed <mesar.hameed@gmail.com>
2020-03-21 20:47:57 +0000

    rewrite goto

--- .github/extra_resources/move_locked_file.bat
+++ .github/extra_resources/move_locked_file.bat
@@ -5,6 +5,6 @@ echo "arg2: %2"
 echo "sleeping for a bit"
 ping 192.0.2.0 -n 1 -w 1000 > nul
 move %1 %2 > null
-IF NOT ERRORLEVEL 0 (goto LOOP)
+IF NOT ERRORLEVEL 0 (goto :LOOP)
 echo "file moved"
 exit /b 0

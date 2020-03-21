4f66793b50d727a6227a816f1267275f01934f31
Mesar Hameed <mesar.hameed@gmail.com>
2020-03-21 18:35:06 +0000

    fix typo and remove echo statements

--- .github/extra_resources/move_locked_file.bat
+++ .github/extra_resources/move_locked_file.bat
@@ -1,9 +1,7 @@
 @ECHO OFF
 :LOOP
-echo "my first argument is: %1"
-echo "my second argument is: %2"
 move %*
-IF NOT ERRORLEVEL 0 (GOTO Finished)
+IF ERRORLEVEL 0 (GOTO Finished)
 ping 192.0.2.0 -n 1 -w 1000 > nul
 GOTO Loop
 :Finished

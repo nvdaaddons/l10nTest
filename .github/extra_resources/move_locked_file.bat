de81940c3e3dd253379578f840902c013b46d4bc
Mesar Hameed <mesar.hameed@gmail.com>
2020-03-21 19:26:41 +0000

    consistancy with lable casing

--- .github/extra_resources/move_locked_file.bat
+++ .github/extra_resources/move_locked_file.bat
@@ -3,10 +3,10 @@ echo "arg1: %1"
 echo "arg2: %1"
 :LOOP
 move %1 %2
-IF ERRORLEVEL 0 (GOTO Finished)
+IF ERRORLEVEL 0 (GOTO FINISHED)
 echo "sleeping for a bit"
 ping 192.0.2.0 -n 1 -w 1000 > nul
-GOTO Loop
-:Finished
+GOTO LOOP
+:FINISHED
 move %1 %2
 echo "file moved"

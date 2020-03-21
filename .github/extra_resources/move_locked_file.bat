6386e85bd9724e2b0f9dd978461d283eb501cfb9
Mesar Hameed <mesar.hameed@gmail.com>
2020-03-21 20:38:17 +0000

    simplify logic

--- .github/extra_resources/move_locked_file.bat
+++ .github/extra_resources/move_locked_file.bat
@@ -2,15 +2,9 @@
 echo "arg1: %1"
 echo "arg2: %2"
 :LOOP
-move %1 %2
-IF ERRORLEVEL 0 (
-  GOTO FINISHED
-) else (
-  echo "sleeping for a bit"
-  ping 192.0.2.0 -n 1 -w 1000 > nul
-  GOTO LOOP
-)
-:FINISHED
-move %1 %2
+echo "sleeping for a bit"
+ping 192.0.2.0 -n 1 -w 1000 > nul
+move %1 %2 > null
+IF NOT ERRORLEVEL 0 (goto LOOP)
 echo "file moved"
 exit /b 0

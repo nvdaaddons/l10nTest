d1c1aca502c765fa35142a6f019368c39c1dbe22
Mesar Hameed <mesar.hameed@gmail.com>
2020-03-21 19:54:32 +0000

    use if else statement

--- .github/extra_resources/move_locked_file.bat
+++ .github/extra_resources/move_locked_file.bat
@@ -3,10 +3,13 @@ echo "arg1: %1"
 echo "arg2: %1"
 :LOOP
 move %1 %2
-IF ERRORLEVEL 0 (GOTO FINISHED)
-echo "sleeping for a bit"
-ping 192.0.2.0 -n 1 -w 1000 > nul
-GOTO LOOP
+IF ERRORLEVEL 0 (
+  GOTO FINISHED
+) else (
+  echo "sleeping for a bit"
+  ping 192.0.2.0 -n 1 -w 1000 > nul
+  GOTO LOOP
+)
 :FINISHED
 move %1 %2
 echo "file moved"

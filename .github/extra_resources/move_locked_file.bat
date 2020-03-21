40b484314385995358636b39fbf82796fe05759f
Mesar Hameed <mesar.hameed@gmail.com>
2020-03-21 19:01:18 +0000

    explisit, and exact arguments

--- .github/extra_resources/move_locked_file.bat
+++ .github/extra_resources/move_locked_file.bat
@@ -1,8 +1,12 @@
 @ECHO OFF
+echo "arg1: %1"
+echo "arg2: %1"
 :LOOP
-move %*
+move %1 %2
 IF ERRORLEVEL 0 (GOTO Finished)
+echo "sleeping for a bit"
 ping 192.0.2.0 -n 1 -w 1000 > nul
 GOTO Loop
 :Finished
-move %*
+move %1 %2
+echo "file moved"

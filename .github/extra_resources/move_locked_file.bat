b9f762626257a7fabb0e1e7b1a3417c67dce28bb
Mesar Hameed <mesar.hameed@gmail.com>
2020-03-22 03:13:00 +0000

    try another way of detecting if the full log file has been written

--- .github/extra_resources/move_locked_file.bat
+++ .github/extra_resources/move_locked_file.bat
@@ -1,10 +1,11 @@
-@ECHO OFF
+::@ECHO OFF
 echo "arg1: %1"
 echo "arg2: %2"
 :LOOP
 echo "sleeping for a bit"
 ping 192.0.2.0 -n 1 -w 1000 > nul
-move %1 %2 > null
+findstr /i /c:"nvda exit" %1
 IF NOT ERRORLEVEL 0 (goto :LOOP)
+copy %1 %2
 echo "file moved"
 exit /b 0

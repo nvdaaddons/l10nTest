be0e6186dca55c717966967197d99edd1c0ccad1
Mesar Hameed <mesar.hameed@gmail.com>
2020-03-22 03:57:10 +0000

    once more .. with ...

--- .github/extra_resources/move_locked_file.bat
+++ .github/extra_resources/move_locked_file.bat
@@ -1,12 +1,11 @@
+@echo off
 echo "arg1: %1"
 echo "arg2: %2"
 :LOOP
 echo "sleeping for a bit"
 ping 192.0.2.0 -n 1 -w 1000 > nul
-findstr /i /c:"nvda exit" %1
-IF ERRORLEVEL 0 GOTO :DONE
-GOTO :LOOP
-:DONE
-move %1 %2
-echo "file moved"
+findstr /i /c:"NVDA exit" %1
+IF %ERRORLEVEL% NEQ 0 GOTO :LOOP
+copy %1 %2
+echo "file copied"
 exit /b 0

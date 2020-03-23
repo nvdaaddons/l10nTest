2e0d2adcd6ff5c5637d142bdada1f2497e9b5a71
Noelia Ruiz Martínez <nrm1977@gmail.com>
2020-03-23 19:42:13 +0100

    Copy log using a plugin inside NVDA

---
+++ .github/extra_resources/globalPlugins/nvda.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# Global plugin for testing NVDA add-ons in GitHub Actions
+# Copyright (C) 2012-2019 Noelia Ruiz Martínez, mesar Hameed
+# Released under GPL 2
+
+import globalPluginHandler
+import globalVars
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+	def terminate(self):
+		newPath = shutil.copy(globalVars.appArgs.logFileName, os.environ['workspace'])
